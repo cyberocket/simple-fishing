f709c80a9dbc7282af1b49a8f2a35d8c28bda95a
Victor Savitsky <savitsky@panzerdog.com>
2018-02-11 03:33:39 +0200

    Added rope joint

---
+++ Assets/Scripts/Game/Gameplay/Rope/RopeJoint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Game.Gameplay {
+
+    public sealed class RopeJoint : MonoBehaviour {
+
+        public float Mass {
+            set { _body.mass = value; }
+            get { return _body.mass; }
+        }
+
+        public float Distance {
+            set { _joint.distance = value; }
+            get { return _joint.distance; }
+        }
+
+        public float Width {
+            set { _collider.radius = value / 2; }
+            get { return _collider.radius * 2; }
+        }
+
+
+        [SerializeField]
+        private Rigidbody2D _body;
+
+        [SerializeField]
+        private SpringJoint2D _joint;
+
+        [SerializeField]
+        private CircleCollider2D _collider;
+
+
+        public void ConnectBody(Rigidbody2D body) {
+            _joint.connectedBody = body;
+            _joint.connectedAnchor = Vector3.zero;
+        }
+    }
+}
